7f5f53821f40ef23e300ace7dd0be35715221334
Todd Henderson <todd@todd-henderson.me>
2026-06-02 17:57:00 -0500

    Add VS Code extension scaffold

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext)
+{
+    console.log('AiLang extension activated');
+}
+
+export function deactivate()
+{
+}
